bc50042fd3d5e897bc38f343eb8e096f86940515
samwhelp <samwhelp@users.noreply.github.com>
2026-01-26 22:43:14 +0800

    fix

--- helper/ext/path.sh
+++ helper/ext/path.sh
@@ -6,3 +6,23 @@
 
 REF_ASSET_DIR_PATH="${REF_SUIT_DIR_PATH}/asset"
 REF_BUILD_DIR_PATH="${REF_SUIT_DIR_PATH}/build"
+
+
+
+
+##
+## ## Path / Asset
+##
+
+REF_ASSET_OVERLAY_DIR_PATH="${REF_ASSET_DIR_PATH}/overlay"
+REF_ASSET_TEMPLATE_DIR_PATH="${REF_ASSET_DIR_PATH}/template"
+
+
+
+
+##
+## ## Path / Master
+##
+
+REF_MASTER_RIVER_DIR_PATH="${REF_BUILD_DIR_PATH}/river"
+REF_MASTER_KWM_DIR_PATH="${REF_BUILD_DIR_PATH}/river-kwm"
