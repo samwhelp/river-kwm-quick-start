27c2526a7428da5a57d5c1c4c1b56b4eb6af2e83
samwhelp <samwhelp@users.noreply.github.com>
2026-01-26 23:28:18 +0800

    fix

--- helper/mak/river.mk
+++ helper/mak/river.mk
@@ -8,3 +8,8 @@ river-source-download:
 river-build-essential:
 	@river-build-essential.sh
 .PHONY: river-build-essential
+
+
+river-build-and-install:
+	@river-build-and-install.sh
+.PHONY: river-build-and-install
