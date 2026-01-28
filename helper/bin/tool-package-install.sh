8bed35ff286a095b42411db5cfab15dba7d0a021
samwhelp <samwhelp@users.noreply.github.com>
2026-01-29 04:58:21 +0800

    fix

--- helper/bin/tool-package-install.sh
+++ helper/bin/tool-package-install.sh
@@ -56,9 +56,9 @@ mod_tool_package_install () {
 sys_tool_package_install () {
 
 	echo
-	echo sudo pacman -Sy --needed xwayland-satellite swaybg polkit-gnome thunar xfce4-terminal xfce4-screenshooter rofi wmenu kitty foot
+	echo sudo pacman -Sy --needed xwayland-satellite swaybg polkit-gnome thunar xfce4-terminal xfce4-screenshooter rofi wmenu kitty foot brightnessctl pamixer
 	echo
-	sudo pacman -Sy --needed xwayland-satellite swaybg polkit-gnome thunar xfce4-terminal xfce4-screenshooter rofi wmenu kitty foot
+	sudo pacman -Sy --needed xwayland-satellite swaybg polkit-gnome thunar xfce4-terminal xfce4-screenshooter rofi wmenu kitty foot brightnessctl pamixer
 
 }
 
